33b894265923441721eeb012f7def500b333aaa6
Akshay Jay <akshayjay1722@gmail.com>
2026-03-11 17:51:38 -0700

    Added firebase and grok functionality for fog layer web app

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ def main():
         epochs=5,
         imgsz=640,
         device=0,                               # '0' uses NVIDIA's GPU. Change to 'cpu' if you don't have one.
-        batch=16,
+        batch=8,
         project="fridge_vision_project",
         name="initial_test_run"
     )
